3783e65fff6a7457449ae43d9f36b1cdbdf422c0
kevinalexis1079 <kevinalexiscastaneda4@gmail.com>
2025-07-08 00:29:19 -0500

    CRUD de usuarios y roles listo con validaciones, relación y restaurar
    CRUD completo de usuarios y roles
    - Registro, listado, edición, eliminación y restauración funcionando correctamente.
    - Validaciones con class-validator para todos los campos: cédula, nombre, apellido, teléfono, correo, contraseña, estado y rol.
    - Se implementó relación ManyToOne entre usuario y rol (id_rol_fk) y se resolvió el problema de null en el registro.
    
    Mejoras:
    - En el listado de usuarios ahora solo se muestra el nombre del rol (no todo el objeto).
    - Se agregó el decorador @DeleteDateColumn y método de restauración con endpoint PATCH `/usuarios/restore/:id`.
    - Mensajes de error claros y personalizados.
    - Se corrigió el enum `estado_usuario` y se aplicó validación con @IsEnum.
    
     Fix:
    - Se solucionó error `UnknownDependenciesException` importando el repositorio de roles correctamente en `UsuariosModule`.
    
     Probado todo y funcional

--- backend-agrotech/src/app.module.ts
+++ backend-agrotech/src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsuariosModule } from './usuarios/usuarios.module';
+import { RolesModule } from './roles/roles.module';
 
 @Module({
-  imports: [],
+  imports:[
+    UsuariosModule,
+    RolesModule,
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host:'localhost',
+      port:5432,
+      username:'agrotech',
+      password:'123',
+      database:'agrotech',
+      autoLoadEntities: true,
+      synchronize:true,
+      retryDelay:3000,
+      retryAttempts:10,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend-agrotech/src/main.ts
+++ backend-agrotech/src/main.ts
@@ -1,8 +1,17 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // 👇 Esto es obligatorio para que los DTOs se validen
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true, // remueve campos que no están en el DTO
+    forbidNonWhitelisted: true, // lanza error si llega un campo no permitido
+    transform: true, // convierte tipos automáticamente (string -> number, etc.)
+  }));
+
+  await app.listen(3000);
 }
 bootstrap();
